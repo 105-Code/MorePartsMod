fa2d4ce88af3952718e28eff018d47852679e0e9
dani0105 <danielrojas0105@gmail.com>
2022-09-29 23:39:54 -0600

    Add MorePartsUnity project
    This project will be used to add modules in Unity

---
+++ MorePartsUnity/MorePartsInjector.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class MorePartsInjector : MonoBehaviour
+{
+
+    public string MinMorePartsVersion = "2.1.1";
+
+    public List<Modules> modules;
+}
+
+public enum Modules{
+    BallonModule,
+    RotorModule,
+    TelecomunicationModule
+}
