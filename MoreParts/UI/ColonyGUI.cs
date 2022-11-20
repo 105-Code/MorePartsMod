ef56856a7e5e5051f03225d8e4eaae48ad452bec
dani0105 <danielrojas0105@gmail.com>
2022-11-19 23:39:57 -0600

    Add refine material section

--- MoreParts/UI/ColonyGUI.cs
+++ MoreParts/UI/ColonyGUI.cs
@@ -1,6 +1,7 @@
 ﻿using MorePartsMod.Buildings;
 using MorePartsMod.Managers;
 using SFS.Input;
+using SFS.UI;
 using SFS.UI.ModGUI;
 using UnityEngine;
 using UnityEngine.UI;
@@ -34,10 +35,17 @@ namespace MorePartsMod.UI
         {
             this._name = this._colony.data.name;
             this._holder = Builder.CreateWindow(this.transform,2, 500, 700,posY:350,titleText:"Colony Menu");
+            this._holder.EnableScrolling(Type.Vertical);
             this._holder.CreateLayoutGroup(Type.Vertical).spacing = 20f;
             this._holder.CreateLayoutGroup(Type.Vertical).DisableChildControl();
             this._holder.CreateLayoutGroup(Type.Vertical).childAlignment = TextAnchor.UpperCenter;
             this.generateUI();
+        }
+
+        private void Reload()
+        {
+            this.OnClose();
+            this.OnOpen();
         }
 
         public override void ProcessInput()
@@ -60,10 +68,10 @@ namespace MorePartsMod.UI
             {
                 Builder.CreateLabel(this._holder.ChildrenHolder, 480, 35, 0, 0, key + ": " +this._colony.data.resources[key]);
             }
-
+            
             Builder.CreateLabel(this._holder.ChildrenHolder, 480, 35, 0, 0, "Buildings");
 
-            foreach(string buildingName in MorePartsModMain.Main.ColonyBuildingFactory.GetBuildingsName())
+            foreach (string buildingName in MorePartsModMain.Main.ColonyBuildingFactory.GetBuildingsName())
             {
                 if (this._colony.data.isBuildingActive(buildingName))
                 {
@@ -72,7 +80,37 @@ namespace MorePartsMod.UI
                 Builder.CreateButton(this._holder.ChildrenHolder, 480, 60, 0, 0, () => this.OnClickButton(buildingName), "Build " + buildingName);
             }
 
-            Builder.CreateButton(this._holder.ChildrenHolder, 480, 60, 40, 0, () => ScreenManager.main.CloseCurrent(), "Close");
+            if (this._colony.data.isBuildingActive(MorePartsTypes.REFINERY_BUILDING))
+            {
+                Builder.CreateLabel(this._holder.ChildrenHolder, 480, 45, 0, 0, "Refine materials");
+                Builder.CreateButton(this._holder.ChildrenHolder, 480, 40, 0, 0, () => this.generateResource(MorePartsTypes.CONSTRUCTION_MATERIAL), "+100 Construction material");
+                Builder.CreateButton(this._holder.ChildrenHolder, 480, 40, 0, 0, () => this.generateResource(MorePartsTypes.ELECTRONIC_COMPONENT), "+100 Electronic material");
+                Builder.CreateButton(this._holder.ChildrenHolder, 480, 40, 0, 0, () => this.generateResource(MorePartsTypes.ROCKET_MATERIAL), "+100 Rocket material");
+            }
+
+            Builder.CreateButton(this._holder.ChildrenHolder, 480, 60, 40, 0, () => ScreenManager.main.CloseCurrent(), "Save");
+        }
+
+        private void generateResource(string resourcesType, int toGenerate = 100)
+        {
+            if (!this._colony.data.resources.ContainsKey(MorePartsTypes.MATERIAL))
+            {
+                MsgDrawer.main.Log("There are not material in the colony");
+                return;
+            }
+            
+            double materialQuantity = this._colony.data.getResource(MorePartsTypes.MATERIAL);
+            if (materialQuantity - toGenerate < 0)
+            {
+                this._colony.data.takeResource(MorePartsTypes.MATERIAL, materialQuantity);
+                this._colony.data.addResource(resourcesType, materialQuantity);
+                this.Reload();
+                return;
+            }
+
+            this._colony.data.takeResource(MorePartsTypes.MATERIAL, toGenerate);
+            this._colony.data.addResource(resourcesType, toGenerate);
+            this.Reload();
         }
 
         private void OnClickButton(string buildingName)
@@ -85,4 +123,3 @@ namespace MorePartsMod.UI
 
     }
 }
-//
