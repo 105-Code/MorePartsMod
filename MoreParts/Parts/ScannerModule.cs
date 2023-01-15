2225208fa58d981faa381a08c7745cc99e761c31
dani0105 <danielrojas0105@gmail.com>
2023-01-14 20:13:25 -0600

    Fix #27

--- MoreParts/Parts/ScannerModule.cs
+++ MoreParts/Parts/ScannerModule.cs
@@ -34,7 +34,7 @@ namespace MorePartsMod.Parts
 
         private void Update()
         {
-            if(GameManager.main == null || !this._active.Value || this.Location == null || this.Location.planet == null)
+            if(GameManager.main == null || !this._active.Value || this.Location == null || this.Location.planet == null || this.Rocket.location == null)
             {
                 return;
             }
