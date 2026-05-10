a2f91cfd7da391af14e0b16c1a08dd974d696703
Daniel Rojas Rodríguez <danielrojas0105@gmail.com>
2026-05-09 23:22:42 -0600

    Improve RotorModule

--- MoreParts/Parts/RotorModule.cs
+++ MoreParts/Parts/RotorModule.cs
@@ -1,4 +1,4 @@
-﻿using SFS;
+using SFS;
 using SFS.Parts;
 using SFS.Translations;
 using SFS.Variables;
@@ -14,121 +14,120 @@ namespace MorePartsMod.Parts
 {
     public class RotorModule : MonoBehaviour, INJ_IsPlayer, INJ_Location, INJ_Throttle, INJ_Physics
     {
-
-        public I_MsgLogger Logger
-        {
-            get
-            {
-                if (!this.IsPlayer)
-                {
-                    return new MsgNone();
-                }
-                return MsgDrawer.main;
-            }
-        }
         public Animator Animator;
         public Bool_Reference IsOn;
         public Float_Reference Throttle_current;
         public Float_Reference RPM;
 
         public Transform Base;
-        public float Throttle { set => this.Throttle_current.Value = value; }
-
-        private const float Radius = 0.6f;
-        private float _area;
-        private float _rotorVelocity;
         public Part Part;
 
-        public Rigidbody2D Rb2d { set; get; }
+        private const float Radius = 0.6f;
+        private float _diskArea;
+        private float _diskVelocity;
 
+        public Rigidbody2D Rb2d { set; get; }
         public Location Location { set; get; }
         public bool IsPlayer { set; get; }
+        public float Throttle { set => Throttle_current.Value = value; }
+
+        public I_MsgLogger Logger => IsPlayer ? (I_MsgLogger)MsgDrawer.main : new MsgNone();
 
         public void Awake()
         {
-            Animator = this.GetComponent<Animator>();
-            this._area = (float)(Math.PI * Radius * Radius);
+            Animator = GetComponent<Animator>();
+            _diskArea = (float)(Math.PI * Radius * Radius);
         }
 
         private void Start()
         {
             if (GameManager.main == null)
             {
-                base.enabled = false;
+                enabled = false;
                 return;
             }
 
-            this.IsOn.OnChange += this.RecalculateRotor_throttle;
-            this.Throttle_current.OnChange += this.RecalculateRotor_throttle;
-            Part.onPartUsed.AddListener(this.Toggle);
+            IsOn.OnChange += RecalculateRotor;
+            Throttle_current.OnChange += RecalculateRotor;
+            Part.onPartUsed.AddListener(Toggle);
         }
 
-        private void RecalculateRotor_throttle()
+        private void RecalculateRotor()
         {
-            Animator.speed = 100 * (float)this.Throttle_current.Value;
-            this._rotorVelocity = (float)(((RPM.Value * this.Throttle_current.Value) / 60) * 2 * Math.PI);
-            if (!this.IsOn.Value)
+            float throttle = (float)Throttle_current.Value;
+            // Angular velocity (rad/s) plugged into the actuator-disk formula as the
+            // through-disk velocity Vd. Treating omega numerically as m/s is a tuning
+            // shortcut: RPM (set per-prefab) is the knob that scales thrust.
+            _diskVelocity = (float)((RPM.Value * throttle / 60.0) * 2.0 * Math.PI);
+
+            Animator.speed = 100f * throttle;
+
+            if (!IsOn.Value)
             {
-                this.Throttle_current.Value = 0f;
+                Throttle_current.Value = 0f;
                 Animator.SetBool("isOn", false);
                 return;
             }
 
-            if (this.Throttle_current.Value > 0)
-            {
-                Animator.SetBool("isOn", true);
-                return;
-            }
-
-            Animator.SetBool("isOn", false);
+            Animator.SetBool("isOn", throttle > 0f);
         }
 
         private void Toggle(UsePartData data)
         {
-            if (this.IsOn.Value)
+            if (IsOn.Value)
             {
-                this.DisableRotor(this.Logger);
+                DisableRotor(Logger);
                 return;
             }
-            this.EnableRotor(this.Logger);
-
+            EnableRotor(Logger);
             data.successfullyUsedPart = true;
         }
 
         private void EnableRotor(I_MsgLogger logger)
         {
-            this.IsOn.Value = true;
-
-            if (this.Throttle_current.Value == 0f)
+            IsOn.Value = true;
+            if (Throttle_current.Value == 0f)
             {
-                logger.Log(Loc.main.Engine_Module_State.InjectField(this.IsOn.Value.State_ToOnOff(), "state"));
+                logger.Log(Loc.main.Engine_Module_State.InjectField(IsOn.Value.State_ToOnOff(), "state"));
             }
         }
 
         private void DisableRotor(I_MsgLogger logger)
         {
-            bool flag = this.Throttle_current.Value == 0f;
-            this.IsOn.Value = false;
-            if (flag)
+            bool wasIdle = Throttle_current.Value == 0f;
+            IsOn.Value = false;
+            if (wasIdle)
             {
-                logger.Log(Loc.main.Engine_Module_State.InjectField(this.IsOn.Value.State_ToOnOff(), "state"));
+                logger.Log(Loc.main.Engine_Module_State.InjectField(IsOn.Value.State_ToOnOff(), "state"));
             }
         }
+
+        // Actuator-disk thrust: T = 0.5 * rho * A * (V_exit^2 - V0^2),
+        // with V_exit = 2*Vd - V0 -- see https://www.grc.nasa.gov/www/k-12/airplane/propth.html
         private void FixedUpdate()
         {
-            if (this.Rb2d == null || !this.IsOn.Value || this.Location.planet == null)
+            if (Rb2d == null || !IsOn.Value || Location == null || Location.planet == null)
             {
                 return;
             }
-            // check https://www.grc.nasa.gov/www/k-12/airplane/propth.html
-            float exitVelocity = (float)((2 * this._rotorVelocity) - this.Location.VerticalVelocity);
-            float density = (float)this.Location.planet.GetAtmosphericDensity(this.Location.GetTerrainHeight(true));
-            double thrust = 0.5 * density * this._area * ((exitVelocity * exitVelocity) - (this.Location.VerticalVelocity * this.Location.VerticalVelocity));
 
-            Vector2 force = Base.transform.TransformVector(Vector2.up * (float)thrust);
-            Vector2 relativePoint = this.Rb2d.GetRelativePoint(Transform_Utility.LocalToLocalPoint(this.transform, this.Rb2d, new Vector2(0, 0)));
-            this.Rb2d.AddForceAtPosition(force, relativePoint, ForceMode2D.Force);
+            float density = (float)Location.planet.GetAtmosphericDensity(Location.Height);
+            if (density <= 0f)
+            {
+                return;
+            }
+
+            Vector2 thrustAxis = Base.up;
+            float v0 = Vector2.Dot(Location.velocity.ToVector2, thrustAxis);
+
+            float vExit = 2f * _diskVelocity - v0;
+            float thrust = 0.5f * density * _diskArea * (vExit * vExit - v0 * v0);
+            if (thrust <= 0f)
+            {
+                return;
+            }
+
+            Rb2d.AddForceAtPosition(thrustAxis * thrust, transform.position, ForceMode2D.Force);
         }
-
     }
 }
